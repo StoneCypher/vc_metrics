da1e2da920e6fb450c77e7c661e2bd418205abdc
John Haugeland <stonecypher@gmail.com>
2015-12-31 15:59:46 -0800

    working build infrastructure

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,27 @@
 
-var targets   = require('./config/targets.json'),
-    dirs      = require('./config/dirs.json'),
+var targets      = require('./config/targets.json'),
+    dirs         = require('./config/dirs.json'),
 
-    babel_cfg = require('./config/babel.json');
+    babel_cfg    = require('./config/babel.json');
 
 
-var fs        = require('fs'),
-    path      = require('path'),
+var fs           = require('fs'),
+    path         = require('path'),
 
-    del       = require('del'),
-    gulp      = require('gulp'),
-    babel     = require('gulp-babel');
+    del          = require('del'),
+    gulp         = require('gulp'),
+
+    browserify   = require('browserify'),
+    source       = require('vinyl-source-stream'),
+    babel        = require('gulp-babel');
+
+
+var production   = false,
+
+    errorHandler = function(err) {
+      console.log(err.toString());
+      this.emit("end");
+    };
 
 
 
@@ -19,6 +30,67 @@ var fs        = require('fs'),
 gulp.task('clean', function(cb) {
   return del([dirs.build], cb);
 });
+
+
+
+
+
+gulp.task('copy', ['make-directories'], function() {
+
+  return gulp.src([targets.html])
+    .pipe(gulp.dest(dirs.built_html));
+
+});
+
+
+
+
+
+gulp.task('arrange', ['build'], function(cb) {
+
+  gulp.src(targets.fe).pipe(gulp.dest(dirs.arrange));
+
+  return cb();
+
+});
+
+
+
+
+
+gulp.task('build', ['browserify', 'copy']);
+
+
+
+
+
+gulp.task('browserify', ['babel'], function() {
+
+  var browserifyConfig = {
+    "entries"    : [dirs.built_js + '/vcm.js'],
+    "extensions" : [".jsx"]
+  };
+
+  var bpack = browserify(browserifyConfig, { "debug" : !production })
+
+    // these should probably be externalized instead
+    // todo: find time for love later, doctor jones
+    .add(dirs.react_npm      + "/react.js",      { "expose" : "react" })
+    .add(dirs.reactdom_npm   + "/react-dom.js",  { "expose" : "react-dom" })
+
+    .add(dirs.built_js       + "/VcmRoot.js",    { "expose" : "VcmRoot" });
+
+//  .add(dirs.sc_react_npm   + "/sc_react.js",   { "expose" : "sc_react" });
+
+  return bpack
+    .require(dirs.built_js   + "/vcm.js",        { "expose" : "vcm" })
+
+    .bundle()
+    .on("error", errorHandler)
+    .pipe(source("vcm_bundle.js"))
+    .pipe(gulp.dest(dirs.built_js));
+
+  });
 
 
 
@@ -51,4 +123,4 @@ gulp.task('babel', ['make-directories'], function() {
 
 
 
-gulp.task('default', ['babel']);
+gulp.task('default', ['arrange']);
---
+++ src/jsx/VcmRoot.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+
+
+
+
+var VcmRoot = React.createClass({
+
+  render: function() { return <div>VcmRoot todo</div>; }
+
+});
+
+
+
+
+
+export default VcmRoot;
